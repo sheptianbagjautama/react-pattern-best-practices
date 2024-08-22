3cca6a398ff44118869dfb5041aa50a41746b74b
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-22 15:34:16 +0700

    Managin Multi-Component State with the Context API

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
           <AccordionItem
+            id="experience"
             className="accordion-item"
             title="We got 20 years of experience"
           >
@@ -20,6 +21,7 @@ function App() {
             </article>
           </AccordionItem>
           <AccordionItem
+            id="local-guides"
             className="accordion-item"
             title="We're working with local guides"
           >
